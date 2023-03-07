15a10ac20f4ab95d135460dd00471b84bf8b8348
Chen Chang <cys19970903@gmail.com>
2023-03-08 01:05:30 +0800

    init commit

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+	extends: ['airbnb', 'airbnb-typescript'],
+	rules: {
+		'react/prop-types': 0,
+		'react/jsx-no-bind': 0,
+		'@typescript-eslint/indent': 0,
+		'no-tabs': 0,
+		'import/no-extraneous-dependencies': 0,
+		'react/jsx-props-no-spreading': 0,
+		'react/function-component-definition': 0,
+		'react/react-in-jsx-scope': 0,
+		'react/no-array-index-key': 'off',
+		'no-nested-ternary': 'off',
+		'react/jsx-no-constructed-context-values': 'off',
+		'react/require-default-props': 'off',
+		'import/prefer-default-export': 'off',
+		'import/no-extraneous-dependencies': 'off',
+		'default-case': 'off',
+		'react/jsx-no-useless-fragment': 'off',
+		'@typescript-eslint/naming-convention': 'off',
+		'comma-dangle': [
+			'error',
+			{
+				arrays: 'always-multiline',
+				objects: 'always-multiline',
+				imports: 'always-multiline',
+				exports: 'always-multiline',
+				functions: 'ignore',
+			},
+		],
+	},
+};
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# eslint-config-react
-Eslint config
+# eslint-config-bebop
+Eslint config for React
+
+## Install
+
+```sh
+yarn add -D eslint-config-bebop
+```
+
+## Usage
+
+In your `.eslintrc.js`:
+
+```tsx
+module.exports = {
+  extends: ['bebop'],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+};
+```
+
+## License
+
+MIT
